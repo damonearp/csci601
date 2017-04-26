9fae7d681653fb714f29b678f67983150ff29922
Damon Earp <earp.dn@gmail.com>
2017-04-26 10:04:59 -0400

    asf

---
+++ edit_train.php
@@ -0,0 +1,66 @@
+<?php
+  $id = $_GET['id'];
+	$conn = new mysqli("localhost", "csci601", "csci601", "csci601");
+        if ($conn->connect_error) {
+                die("Connection failed: ". $conn->connect_error);
+        }
+  $attrs = $conn->query("select name,capacity,speed from train where id=$id")->fetch_array();
+  $name = $attrs[0];
+  $capacity = $attrs[1];
+  $speed = $attrs[2];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Book A Choooo Chooooo</title>
+        <style>
+            body {
+                font-family: "Courier New", monospace;
+                font-size: small;
+            }
+
+            .insert {
+                padding: 5px;
+                border-style: solid;
+                border-width: 1px;
+                margin-bottom: 5px;
+            }
+
+            .insert p {
+                font-weight: bold;
+                color: red;
+            }
+
+            .insert form {
+                margin-bottom: 5px;
+            }
+
+            .insert th {
+                color: red;
+            }
+
+            th, td {
+                text-align: center;
+                border-bottom: 1px solid #bbb;
+            }
+
+            td {
+                text-align: left;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="insert">
+            <p>Edit Train</p>
+            <form method="POST" action="update_train.php">
+                <input type="hidden" name="train_id" value="<?php echo "$id"; ?>" />
+                <label>Name: <input type="text" name="train_name" value="<?php echo "$name"; ?>" /></label>
+                <label>Name: <input type="text" name="train_capacity" value="<?php echo "$capacity"; ?>" /></label>
+                <label>Name: <input type="text" name="train_speed" value="<?php echo "$speed"; ?>" /></label>
+                <input type="submit" />
+            </form>
+        </div>
+    </body>
+</html>
--- update_train.php
+++ update_train.php
@@ -1,22 +1,24 @@
 <?php
-	$update_station = "UPDATE station SET name = ? WHERE id = ?";
+	$update_train = "UPDATE train SET name = ?, capacity = ?, speed = ? WHERE id = ?";
 	$conn = new mysqli("localhost", "csci601", "csci601", "csci601");
         if ($conn->connect_error) {
                 die("Connection failed: ". $conn->connect_error);
         }
 
-  $id = $_POST['station_id'];
-	$name = $_POST['station_name'];
+  $id = $_POST['train_id'];
+	$name = $_POST['train_name'];
+	$name = $_POST['train_capacity'];
+	$name = $_POST['train_speed'];
 
-	$stmt = $conn->prepare($update_station);
+	$stmt = $conn->prepare($update_train);
 	if (!$stmt) {
 		die("Prepare failed: " . $conn->error);
 	}
-	if (!$stmt->bind_param("si", $name, $id)) {
+	if (!$stmt->bind_param("siii", $name, $capacity, $speed, $id)) {
 		die("Prepare bind failed: " . $stmt->error);
 	}
 	if (!$stmt->execute()) {
 		die("Execute failed: " . $stmt->error);
 	}
-	header("Location: index.php#startions");
+	header("Location: index.php#trains");
 ?>
