9d843f08c5826ecd6e4c24682d79e7e85cfec352
Damon Earp <earp.dn@gmail.com>
2017-04-26 09:58:47 -0400

    asd

--- edit_station.php
+++ edit_station.php
@@ -1,21 +1,61 @@
 <?php
-	$insert_station = "INSERT INTO station (name) VALUES (?)";
+  $station_id = $_GET['id'];
 	$conn = new mysqli("localhost", "csci601", "csci601", "csci601");
         if ($conn->connect_error) {
                 die("Connection failed: ". $conn->connect_error);
         }
-	
-	$name = $_POST['station_name'];
-	
-	$stmt = $conn->prepare($insert_station);
-	if (!$stmt) {
-		die("Prepare failed: " . $conn->error);
-	}
-	if (!$stmt->bind_param("s", $name)) {
-		die("Prepare bind failed: " . $stmt->error);		
-	}
-	if (!$stmt->execute()) {
-		die("Execute failed: " . $stmt->error);
-	}
-	header("Location: index.php");
+  $station_name = $conn->query("select name from station where id=$station_id")->fetch_array()[0];
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Book A Choooo Chooooo</title>
+        <style>
+            body {
+                font-family: "Courier New", monospace;
+                font-size: small;
+            }
+
+            .insert {
+                padding: 5px;
+                border-style: solid;
+                border-width: 1px;
+                margin-bottom: 5px;
+            }
+
+            .insert p {
+                font-weight: bold;
+                color: red;
+            }
+
+            .insert form {
+                margin-bottom: 5px;
+            }
+
+            .insert th {
+                color: red;
+            }
+
+            th, td {
+                text-align: center;
+                border-bottom: 1px solid #bbb;
+            }
+
+            td {
+                text-align: left;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="insert">
+            <p>Edit Station</p>
+            <form method="POST" action="update_station.php">
+                <input type="hidden" name="station_id" value="<?php echo "$station_id"; ?>" />
+                <label>Name: <input type="text" name="station_name" value="<?php echo "$station_name"; ?>" /></label>
+                <input type="submit" />
+            </form>
+        </div>
+    </body>
+</html>
--- update_station.php
+++ update_station.php
@@ -1,18 +1,19 @@
 <?php
-	$insert_station = "INSERT INTO station (name) VALUES (?)";
+	$update_station = "UPDATE station SET name = ? WHERE id = ?";
 	$conn = new mysqli("localhost", "csci601", "csci601", "csci601");
         if ($conn->connect_error) {
                 die("Connection failed: ". $conn->connect_error);
         }
-	
+
+  $id = $_POST['station_id'];
 	$name = $_POST['station_name'];
-	
-	$stmt = $conn->prepare($insert_station);
+
+	$stmt = $conn->prepare($update_station);
 	if (!$stmt) {
 		die("Prepare failed: " . $conn->error);
 	}
-	if (!$stmt->bind_param("s", $name)) {
-		die("Prepare bind failed: " . $stmt->error);		
+	if (!$stmt->bind_param("si", $name, $id)) {
+		die("Prepare bind failed: " . $stmt->error);
 	}
 	if (!$stmt->execute()) {
 		die("Execute failed: " . $stmt->error);
